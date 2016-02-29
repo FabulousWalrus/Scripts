3fbbf15415ee841e9c346b58969a7550b0f8be87
FabulousWalrus <fabulouswalrus329@gmail.com>
2016-02-29 09:09:23 -0500

    Random color and number generator.
    Careful before you watch.

---
+++ Seizures.bat
@@ -0,0 +1,14 @@
+@echo off
+:start
+color 1a
+echo %random% %random% %random% %random% %random% %random% %random% %random% 
+color 2b
+echo %random% %random% %random% %random% %random% %random% %random% %random% 
+color 3c
+echo %random% %random% %random% %random% %random% %random% %random% %random%
+color 4d
+echo %random% %random% %random% %random% %random% %random% %random% %random% 
+color 5e
+echo %random% %random% %random% %random% %random% %random% %random% %random% 
+color 6f
+goto start
